b00fefb62f6b515f0aaf0640224eb47ff3856fc5
Ardnahcivar01 <134200599+Ardnahcivar01@users.noreply.github.com>
2023-05-23 17:31:53 +0530

    Update search_In_BST_Tree.c

--- search_In_BST_Tree.c
+++ search_In_BST_Tree.c
@@ -1,3 +1,4 @@
+// Program for searching in Binary search tree using c language
 #include<stdio.h>
 #include<malloc.h>
 
@@ -8,12 +9,12 @@ struct node{
 };
 
 struct node* createNode(int data){
-    struct node *n; // creating a node pointer
-    n = (struct node *) malloc(sizeof(struct node)); // Allocating memory in the heap
-    n->data = data; // Setting the data
-    n->left = NULL; // Setting the left and right children to NULL
-    n->right = NULL; // Setting the left and right children to NULL
-    return n; // Finally returning the created node
+    struct node *n; 
+    n = (struct node *) malloc(sizeof(struct node)); 
+    n->data = data; 
+    n->left = NULL;
+    n->right = NULL; 
+    return n;
 }
 
 void preOrder(struct  node* root){
@@ -73,20 +74,13 @@ struct node* search(struct node* root,int key){
 }
 int main(){
      
-    // Constructing the root node - Using Function (Recommended)
+   
     struct node *p = createNode(5);
     struct node *p1 = createNode(3);
     struct node *p2 = createNode(34);
     struct node *p3 = createNode(1);
     struct node *p4 = createNode(4);
-    // Finally The tree looks like this:
-    //      5
-    //     / \
-    //    3   6
-    //   / \
-    //  1   4  
-
-    // Linking the root node with left and right children
+   
     p->left = p1;
     p->right = p2;
     p1->left = p3;
